3d51797381eed1a1d83e6ee9fc477b29bb430aca
Maksim <max-goryachev@ya.ru>
2023-10-02 16:09:58 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
 import Component from './component';
-import { useState } from 'react';
-function App() {  
+function App() {
+  const colorPurple = "rgb(150, 150, 252)";
+  const colorOrange = "#ffc559";
+
   return (
-  <div>
-  </div>
+    <div className='container'>
+      <Component colorOne={colorPurple} colorTwo={'black'} />
+      <Component colorOne={colorOrange} colorTwo={'black'} />
+    </div>
   );
-  }
+}
 
 export default App;
--- src/component.jsx
+++ src/component.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react'
 import './component.css'
-function Component(){
-    
-    return(
+import { Data } from './data'
+function Component({ colorOne, colorTwo }) {
+    const [color, setColor] = useState(true)
+    const colors = {
+        backgroundColor: color ? colorOne : colorTwo,
+        color: color ? colorTwo : colorOne,
+    }
+    return (
         <div className='container'>
-            
-
+            <div
+                className="card-wrapper"
+                style={colors}
+            >
+                <div className="card-header">
+                    <h2 className="card-title">{Data.title}</h2>
+                    <h3 className="card-subtitle">{Data.subtTitle}</h3>
+                </div>
+                <div className="card-body">
+                    <img className='image' src={require(`${Data.image}`)} alt="machu" />
+                    <div className="card-body">{Data.cardBody}</div>
+                    <button
+                        style={colors}
+                        onClick={() => setColor(!color)}
+                        className='card-button'
+                    >Change color</button>
+                </div>
+            </div>
         </div>
     )
 }
