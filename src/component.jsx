cb81214a12df718c9a5ed75650435b7b3f6417c0
Maksim <max-goryachev@ya.ru>
2023-10-02 14:56:13 +0600

    create app

---
+++ src/component.jsx
@@ -0,0 +1,11 @@
+import './component.css'
+function Component(){
+    
+    return(
+        <div className='container'>
+            
+
+        </div>
+    )
+}
+export default Component
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const Data = {
+    title: "card-title",
+    subtTitle: 'sub-title',
+    cardBody: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere fugiat enim veniam sequi! Ducimus, sapiente!',
+    image: './images/machu.jpeg'
+}
